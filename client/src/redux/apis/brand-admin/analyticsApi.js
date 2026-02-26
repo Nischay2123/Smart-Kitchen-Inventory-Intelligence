c5b1075d2e15d91b1a9d21826b6d55bffc19d6aa
Nischay2123 <nischaysharma04@gmail.com>
2026-02-26 18:01:01 +0530

    feat: implement item snapshot and live report functionality; enhance daily snapshot processing

--- client/src/redux/apis/brand-admin/analyticsApi.js
+++ client/src/redux/apis/brand-admin/analyticsApi.js
@@ -10,21 +10,6 @@ export const Analytics = baseApi.injectEndpoints({
       }),
     }),
 
-    getDeploymentData: builder.query({
-      query: ({ from, to }) => ({
-        url: "analytics/reports/deployment-snapshot",
-        method: "GET",
-        params: { fromDate: from, toDate: to },
-      }),
-    }),
-    getLiveDeploymentData: builder.query({
-      query: ({ from, to, outletIds }) => ({
-        url: "analytics/reports/deployment-snapshot",
-        method: "GET",
-        params: { fromDate: from, toDate: to },
-        body: { outletIds }
-      }),
-    }),
     getSnapshotDeploymentData: builder.mutation({
       query: ({ outletIds, from, to }) => ({
         url: "analytics/reports/deployment-snapshot",
@@ -50,22 +35,38 @@ export const Analytics = baseApi.injectEndpoints({
       }),
     }),
     getMenuMatrixData: builder.query({
-      query: ({ from, to }) => ({
+      query: ({ from, to, outletId }) => ({
         url: "analytics/menu-matrix",
         method: "GET",
+        params: { fromDate: from, toDate: to, outletId },
+      }),
+    }),
+    getItemSnapshotData: builder.mutation({
+      query: ({ outletId, from, to }) => ({
+        url: "analytics/reports/item-snapshot",
+        method: "POST",
         params: { fromDate: from, toDate: to },
+        body: { outletId },
       }),
     }),
 
+    getItemLiveData: builder.mutation({
+      query: ({ outletId }) => ({
+        url: "analytics/reports/item-live",
+        method: "POST",
+        body: { outletId },
+      }),
+    }),
 
   }),
 });
 
 export const {
   useGetAllOutletsQuery,
-  useGetDeploymentDataQuery,
   useGetMenuMatrixDataQuery,
   useGetMenuItemDataQuery,
   useGetLiveDeploymentDataMutation,
-  useGetSnapshotDeploymentDataMutation
+  useGetSnapshotDeploymentDataMutation,
+  useGetItemSnapshotDataMutation,
+  useGetItemLiveDataMutation,
 } = Analytics;
--- server/src/routes/analytic.routes.js
+++ server/src/routes/analytic.routes.js
@@ -1,15 +1,17 @@
 import express from "express";
 import { verifyJwt } from "../middlerwares/auth.middleware.js";
-import { itemsProfitPerDeployement, menuEngineeringMatrix, ingredientUsageAndBurnRate, getOutlets, brandAnalyticsSnapshotReport, brandAnalyticsLiveReport } from "../controllers/analytic.controller.js";
+import { menuEngineeringMatrix, ingredientUsageAndBurnRate, getOutlets, brandAnalyticsSnapshotReport, brandAnalyticsLiveReport, itemSnapshotReport, itemLiveReport } from "../controllers/analytic.controller.js";
 
 
 
 const router = express.Router();
 
-router.get("/profit", verifyJwt, itemsProfitPerDeployement);
+// router.get("/profit", verifyJwt, itemsProfitPerDeployement);
 router.get("/ingredients/usage", verifyJwt, ingredientUsageAndBurnRate);
 router.post("/reports/deployment-snapshot", verifyJwt, brandAnalyticsSnapshotReport);
 router.post("/reports/deployment-live", verifyJwt, brandAnalyticsLiveReport);
+router.post("/reports/item-snapshot", verifyJwt, itemSnapshotReport);
+router.post("/reports/item-live", verifyJwt, itemLiveReport);
 router.get("/menu-matrix", verifyJwt, menuEngineeringMatrix);
 router.get("/outlets", verifyJwt, getOutlets);
 
